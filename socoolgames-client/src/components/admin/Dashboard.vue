45a0cf98e4702875509f0159fa37009b4aa15298
Jose Pereira <jose.g.pereira@gmail.com>
2026-02-26 12:52:56 +0000

    added: admin base controls

--- socoolgames-client/src/components/admin/Dashboard.vue
+++ socoolgames-client/src/components/admin/Dashboard.vue
@@ -1,14 +1,17 @@
 <template>
-    dashboard
+  <AdminSidebar></AdminSidebar>
+  <AdminMain></AdminMain>
 </template>
 <script lang="ts">
-// import TopBar from "@/components/TopBar.vue";
-// import Footer from "@/components/Footer.vue";
-// //import { useRoute } from "vue-router";
-// import { reactive, onMounted, onUnmounted } from "vue";
 
-export default {
-  name: "DashboardComponent"
-}
+  import AdminSidebar from './AdminSidebar.vue';
+  import AdminMain from './AdminMain.vue';
+  export default {
+    name: "DashboardComponent",
+    components: {
+      AdminSidebar,
+      AdminMain
+    }
+  }
 
 </script>
