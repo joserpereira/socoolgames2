915c5586e5aeeeb3143aa36389ab8951bf6c5e4b
Jose Pereira <jose.g.pereira@gmail.com>
2026-03-17 19:23:08 +0000

    added: remove button in multiplier

--- socoolgames-client/src/components/admin/cms/schemas/WidgetMultiplier.vue
+++ socoolgames-client/src/components/admin/cms/schemas/WidgetMultiplier.vue
@@ -3,13 +3,25 @@
     <div class="p-2 flex items-center">
       <span class="text-center flex-1">Items</span>
       <span class="flex-none">
-        <button type="button" class="bg-primary px-4 py-2 rounded-full text-white hover:bg-secondary hover:text-black"
-                @click.prevent="addItem"
-                >Add Item</button>
+        <button type="button" class="flex items-center text-xs border px-4 py-2 rounded-full hover:bg-primary hover:text-white"
+                @click.prevent="addItem">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 pe-2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+
+                Add Item</button>
       </span>
     </div>
-    <div class="mt-3" v-for="(item, index) in data.value" :key="index">
+    <div class="mt-3 relative pt-5" v-for="(item, index) in data.value" :key="item">
       <ComponentList :schema="props.schema" :data="item" :index="index+1" :componentCache="props.componentCache" :selectedLang="props.selectedLang"></ComponentList>
+       <!-- Botão sobreposto -->
+      <button type="button" class="flex items-center absolute border text-xs top-2 right-2 z-10 px-4 py-2 rounded-full hover:bg-red-500 hover:text-white"
+              @click.prevent="removeItem(index)">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 pe-2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+        </svg>
+        Remove
+    </button>
     </div>
   </div>
 </template>
@@ -24,21 +36,27 @@
         componentCache: {} as Record<string, any>,
         selectedLang: String,
         data: {} as Object
-   })
-   onMounted(() => {
-     data.value = props.value;
-   })    
+  })
+  
+  onMounted(() => {
+    data.value = props.value;
+  })    
 
-   watch(() => props.value, () => {
-     data.value = props.value;
-   })
+  watch(() => props.value, () => {
+    data.value = props.value;
+  })
 
-   const data = reactive({
-     value: [] as any[]
-   })
+  const data = reactive({
+    value: [] as any[]
+  })
 
-   const addItem = () => {
-     var defaults = componentsUtils.getDefaultFromSchema(props.schema, ["en"]);
-     data.value.push(defaults);
-   }
+  const addItem = () => {
+    var defaults = componentsUtils.getDefaultFromSchema(props.schema, ["en"]);
+    data.value.push(defaults);
+  }
+  
+  const removeItem = (index) => {
+    data.value.splice(index, 1)
+  }
+
 </script>
