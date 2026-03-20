8f954f0797f4aee5d9663c4e8b2f9656b86c6dcc
Jose Pereira <jose.g.pereira@gmail.com>
2026-03-20 19:28:25 +0000

    fixed: vertical alignment in cards

--- socoolgames-client/src/components/admin/cms/pageBuilder/BlockMultipleCards.vue
+++ socoolgames-client/src/components/admin/cms/pageBuilder/BlockMultipleCards.vue
@@ -10,22 +10,21 @@
                 </p>
             </div>
 
-            <div :class="props.data.items.length % 4 == 0 ? 'md:grid-cols-4' : 'md:grid-cols-3'" class="grid gap-8">                
+            <div :class="props.data.items.length % 4 == 0 ? 'md:grid-cols-4' : 'md:grid-cols-3'" class="grid gap-8 items-stretch">                
                 <article v-for="(item, index) in props.data.items" :key="index"
-                    class="bg-white hover:scale-105 rounded-3xl shadow-md overflow-hidden hover:shadow-xl transition">
+                    class="flex flex-col bg-white hover:scale-105 rounded-3xl shadow-md overflow-hidden hover:shadow-xl transition">
                     <img :src="formatUrl(baseUrl+item?.image?.thumb)" alt="Jogo Caça Palavras" 
                         :class="props.data.items.length % 4 == 0 ? 'h-48' : 'h-100'" 
                         class="w-full object-cover" loading="lazy">
-                    <div class="p-6">
-                        <span v-if="item.header?.[props.selectedLang]" 
-                            :class="(item.headerColor ?? '').length > 0 ? item.headerColor : 'bg-yellow-100 text-yellow-700'"
-                            class="text-xs px-3 py-1 rounded-full">{{ item.header?.[props.selectedLang] }}</span>
-                        
-                        <h3 v-if="item.title?.[props.selectedLang]" class="text-xl font-bold mt-4 mb-2">{{ item.title?.[props.selectedLang] }}</h3>
-                        <p v-if="item.subTitle?.[props.selectedLang]" class="text-gray-600 mb-4">{{ item.subTitle?.[props.selectedLang] }}</p>
+                    <span v-if="item.header?.[props.selectedLang]" 
+                        :class="(item.headerColor ?? '').length > 0 ? item.headerColor : 'bg-yellow-100 text-yellow-700'"
+                        class="w-fit mx-6 mt-6 text-xs px-3 py-1 rounded-full">{{ item.header?.[props.selectedLang] }}</span>
+                    <h3 v-if="item.title?.[props.selectedLang]" class="px-6 text-xl font-bold mt-4 mb-2">{{ item.title?.[props.selectedLang] }}</h3>
+                    <p v-if="item.subTitle?.[props.selectedLang]" class="flex-1 px-6 flex-1 text-gray-600 mb-4">{{ item.subTitle?.[props.selectedLang] }}</p>
+                    <div class="px-6 pb-6">
                         <a :href="item.buttonLink?.[props.selectedLang]" v-if="item.buttonText?.[props.selectedLang]"
                         :class="(item.buttonColor ?? '').length > 0 ? item.buttonColor : 'bg-primary text-white'"
-                        class="block text-center py-3 rounded-full font-semibold hover:bg-darkgreen transition">
+                        class="mt-auto block text-center py-3 rounded-full font-semibold hover:bg-darkgreen transition">
                             {{ item.buttonText?.[props.selectedLang] }}
                         </a>
                     </div>
