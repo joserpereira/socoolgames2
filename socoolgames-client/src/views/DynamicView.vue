88cd703131c05a79455955a688bebbe78aef0e40
Jose Pereira <jose.g.pereira@gmail.com>
2026-03-31 17:31:44 +0100

    fixed: reditect page

--- socoolgames-client/src/views/DynamicView.vue
+++ socoolgames-client/src/views/DynamicView.vue
@@ -4,20 +4,30 @@
 
 <script setup lang="ts">
     import DynamicPage from '@/components/admin/cms/pages/DynamicPage.vue';
+    import { languages } from '../locales/index'
     import { onMounted, reactive } from 'vue'
-    import { useRoute } from 'vue-router';
+    import { useRoute, useRouter } from 'vue-router';
     
     const route = useRoute();
-    
+    const router = useRouter()
+
     const data = reactive({        
         slugText: '' as String,
-        language: '' as String
+        language: '' as String,
+        languages: ["en", "pt"],
     })
 
-    
     onMounted(() => {   
-        data.slugText = (route.params.slug ?? "").length > 0 ? route.params.slug : "home";
-        data.language = fixLang(route.params.lang);
+        const lang = route.params.lang as string;
+        if (!Object.keys(languages).includes(lang)) {
+            
+            router.push('/'+localStorage.selectedLanguage+'/'+route.params.lang);
+            data.slugText = route.params.lang as string;
+            data.language = localStorage.selectedLanguage;
+        } else {
+            data.slugText = (route.params.slug ?? "").length > 0 ? route.params.slug : "home";
+            data.language = fixLang(route.params.lang);
+        }
     })
     
     const fixLang = (language) => {
