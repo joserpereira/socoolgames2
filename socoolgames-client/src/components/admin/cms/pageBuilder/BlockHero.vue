f0ce944c2b628185409a3c98a5e1e0d451526d13
Jose Pereira <jose.g.pereira@gmail.com>
2026-04-01 19:04:47 +0100

    update hero in update component

--- socoolgames-client/src/components/admin/cms/pageBuilder/BlockHero.vue
+++ socoolgames-client/src/components/admin/cms/pageBuilder/BlockHero.vue
@@ -3,18 +3,19 @@
         <div class="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-12 items-center" >
 
             <div>
-                <h1 class="text-4xl md:text-5xl font-black mb-6 leading-tight">
+                <h1 class="bg-green-500/40 text-4xl backdrop-blur-sm md:text-5xl font-black mb-6 leading-tight rounded-lg px-6 py-4 inline-block">
                     {{ props.data.title?.[props.selectedLang] }}
                 </h1>
 
-                <p class="text-lg text-gray-700 mb-8" v-html="formatText(props.data.subTitle?.[props.selectedLang])">
+                <p class="bg-white/30 backdrop-blur-sm text-lg text-gray-700 mb-8 px-6 rounded-lg py-4 inline-block" v-html="formatText(props.data.subTitle?.[props.selectedLang])">
                     
                 </p>
-
-                <a :href="props.data?.buttonLink?.[props.selectedLang]" 
-                    class="hover:scale-105 bg-primary text-white px-8 py-4 rounded-full font-bold hover:bg-darkgreen transition">
-                    {{ props.data?.buttonText?.[props.selectedLang] }}
-                </a>
+                <div>
+                    <a :href="props.data?.buttonLink?.[props.selectedLang]" 
+                        class="hover:scale-105 bg-primary text-white px-8 py-4 rounded-full inline-block font-bold hover:bg-darkgreen transition">
+                        {{ props.data?.buttonText?.[props.selectedLang] }}
+                    </a>
+                </div>
             </div>
         </div>
     </section>
