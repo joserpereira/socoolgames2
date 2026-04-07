a9fa74518c5bd88c9cfe269b67a68080a684ffbd
Jose Pereira <jose.g.pereira@gmail.com>
2026-04-07 20:18:50 +0100

    added: WidgetBoolean and WidgetOption

---
+++ socoolgames-client/src/components/admin/cms/schemas/WidgetBoolean.vue
@@ -0,0 +1,29 @@
+<template>
+    <label class="block font-medium text-start text-sm mt-2">
+        {{ props.schema.label }} {{ props.index ? '#' + props.index : '' }}
+    </label>
+    <div class="text-start">
+      <input type="checkbox"
+            class="border rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
+            v-model="data.value"
+        />
+    </div>
+</template>
+<script setup lang="ts">
+  import { defineProps, onMounted, reactive } from 'vue'
+
+  const props = defineProps({        
+        schema: Object,        
+        value: Object,
+        index: Number,
+        selectedLang: String
+   })
+
+  onMounted(() => {
+    data.value = props.value ?? false;    
+  })    
+
+  const data = reactive({
+    value: {} as any    
+  })
+</script>
---
+++ socoolgames-client/src/components/admin/cms/schemas/WidgetOption.vue
@@ -0,0 +1,36 @@
+<template>
+    <label class="block font-medium text-start text-sm mt-2">
+        {{ props.schema.label }} {{ props.index ? '#' + props.index : '' }}
+    </label>
+
+    <!-- <input type="text"
+           class="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
+           v-model="data.value"
+      /> -->
+
+    <select name="option" id="option"
+            class="input rounded px-2 py-3 mt-4 peer w-full border">    
+        <option v-for="value in props.schema.options" :value="value" :key="value">
+            {{ value }}
+        </option>
+    </select>
+</template>
+<script setup lang="ts">
+  import { defineProps, onMounted, reactive } from 'vue'
+
+  const props = defineProps({        
+        schema: Object,        
+        value: Object,
+        index: Number,
+        selectedLang: String
+   })
+
+  onMounted(() => {
+    data.value = props.value ?? "";
+    console.log("mounted", props);
+  })    
+
+  const data = reactive({
+    value: {} as any    
+  })
+</script>
