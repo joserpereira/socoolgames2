74b6b194b1b9105e29f1d0a2eb17839c29b3944b
Jose Pereira <jose.g.pereira@gmail.com>
2026-04-14 17:49:44 +0100

    changed: allow to reorder menus

--- socoolgames-client/src/components/admin/cms/MenusEdit.vue
+++ socoolgames-client/src/components/admin/cms/MenusEdit.vue
@@ -44,59 +44,73 @@
                 </span>
             </div>
         </div>
-        <div v-for="(item, index) in data.item.items" :key="index" >
-            <div class="relative mt-4 mx-8">
-                <button type="button" class="flex items-center absolute border text-xs top-2 right-2 z-10 px-4 py-2 rounded-full hover:bg-red-500 hover:text-white"
-                        @click.prevent="removeItem(index)">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 pe-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
-                    </svg>
-                    Remove
-                </button>
-                <label class="block font-medium text-start text-sm mt-2">
-                    Text #{{ index + 1 }}
-                </label>
-                <input type="text"
-                    class="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
-                    v-model="(item || {}).text[data.currentLang]"
-                /> 
+        <draggable class="drag-container"
+                    :list="data.item.items"
+                   handle=".cursor-move">
+            <div class="dragArea" v-for="(item, index) in data.item.items" :key="index" >
+                <div class="relative mt-4 mx-8">
+                    <button type="button" class="flex items-center absolute border text-xs top-2 right-2 z-10 px-4 py-2 rounded-full hover:bg-red-500 hover:text-white"
+                            @click.prevent="removeItem(index)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 pe-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                        </svg>
+                        Remove
+                    </button>
+                    
+                    <label class="block m-1 flex font-medium text-sm mt-1 justify-items-center gap-1 items-center text-start rounded-xl">
+                        <span class="cursor-move hover:bg-gray-300 p-1 rounded-xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                            </svg>
+                        </span>
+                        <span>
+                            Text #{{ index + 1 }}
+                        </span>
+                    </label>
+                    <input type="text"
+                        class="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
+                        v-model="(item || {}).text[data.currentLang]"
+                    /> 
+                </div>
+                <div class="relative mt-4 mx-8">
+                    
+                    <label class="block font-medium text-start text-sm mt-2">
+                        Link #{{ index + 1 }}
+                    </label>
+                    <input type="text"
+                        class="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
+                        v-model="(item || {}).link[data.currentLang]"
+                    />
+                </div>
+                <div class="relative mt-4 mx-8 flex bg-white py-3 px-3 rounded-xl">
+                    <span>Button #{{ index + 1 }}</span>
+                    <div class="timeslot-1 ps-3">
+                        <input type="radio" :name="'button'+index" :id="'Normal'+index" value="0" v-model="(item || {}).button" />
+                        <label class="ps-2" :for="'Normal'+index" >Normal</label>
+                    </div>
+                    <div class="timeslot-2 ps-3">
+                        <input type="radio" :name="'button'+index" :id="'Highlight'+index" value="1" v-model="(item || {}).button"  />
+                        <label :for="'Highlight'+index" class="ps-2 text-primary hover:text-dark mt-2 hover:underline">Highlight</label>
+                    </div>
+                    <div class="timeslot-2 ps-3">
+                        <input type="radio" :name="'button'+index" :id="'Button'+index" value="2" v-model="(item || {}).button"  />
+                        <label :for="'Button'+index" class="ms-2 bg-primary text-white px-4 py-2 rounded-full hover:bg-darkgreen hover:underline">Button</label>
+                    </div>
+                </div>
+                <div class="relative mt-4 mx-8 flex bg-white py-3 px-3 rounded-xl">
+                    <span>Active #{{ index + 1 }}</span>
+                    <div class="timeslot-1 ps-3">
+                        <input type="checkbox" name="activeHeader" :id="'Header'+index" v-model="(item || {}).activeHeader" />
+                        <label class="ps-2" :for="'Header'+index" >Header</label>
+                    </div>
+                    <div class="timeslot-2 ps-3">
+                        <input type="checkbox" name="activeFooter" :id="'Footer'+index" v-model="(item || {}).activeFooter"  />
+                        <label class="ps-2" :for="'Footer'+index">Footer</label>
+                    </div>
+                </div>
             </div>
-            <div class="relative mt-4 mx-8">
-                <label class="block font-medium text-start text-sm mt-2">
-                    Link #{{ index + 1 }}
-                </label>
-                <input type="text"
-                    class="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-500"
-                    v-model="(item || {}).link[data.currentLang]"
-                />
-            </div>
-            <div class="relative mt-4 mx-8 flex bg-white py-3 px-3 rounded-xl">
-                <span>Button #{{ index + 1 }}</span>
-                <div class="timeslot-1 ps-3">
-                    <input type="radio" :name="'button'+index" :id="'Normal'+index" value="0" v-model="(item || {}).button" />
-                    <label class="ps-2" :for="'Normal'+index" >Normal</label>
-                </div>
-                <div class="timeslot-2 ps-3">
-                    <input type="radio" :name="'button'+index" :id="'Highlight'+index" value="1" v-model="(item || {}).button"  />
-                    <label :for="'Highlight'+index" class="ps-2 text-primary hover:text-dark mt-2 hover:underline">Highlight</label>
-                </div>
-                <div class="timeslot-2 ps-3">
-                    <input type="radio" :name="'button'+index" :id="'Button'+index" value="2" v-model="(item || {}).button"  />
-                    <label :for="'Button'+index" class="ms-2 bg-primary text-white px-4 py-2 rounded-full hover:bg-darkgreen hover:underline">Button</label>
-                </div>
-            </div>
-            <div class="relative mt-4 mx-8 flex bg-white py-3 px-3 rounded-xl">
-                <span>Active #{{ index + 1 }}</span>
-                <div class="timeslot-1 ps-3">
-                    <input type="checkbox" name="activeHeader" :id="'Header'+index" v-model="(item || {}).activeHeader" />
-                    <label class="ps-2" :for="'Header'+index" >Header</label>
-                </div>
-                <div class="timeslot-2 ps-3">
-                    <input type="checkbox" name="activeFooter" :id="'Footer'+index" v-model="(item || {}).activeFooter"  />
-                    <label class="ps-2" :for="'Footer'+index">Footer</label>
-                </div>
-            </div>
-        </div>
+        </draggable>
+
         <div class="mt-4">
             <div class="py-2 text-red-400" v-if="data.error">{{ data.error }}</div>
         </div>
@@ -112,12 +126,15 @@
 <script setup lang="ts">
     import service from "@/services/menus.service";
     import { watch, defineProps, onMounted, reactive } from 'vue'
+    import { VueDraggableNext } from 'vue-draggable-next'
 
     const props = defineProps({
         item: Object,
         saved: Function
     })
 
+    const draggable = VueDraggableNext;
+    
     watch(() => props.item, (value) => {        
         if (props?.item && value) {                        
             data.item = value;
