1a67f2da2f17f3054930c37dc3c548e615d7b2c9
Jose Pereira <jose.g.pereira@gmail.com>
2026-04-21 13:32:11 +0100

    fixed: layout page not found and added back button

--- socoolgames-client/src/components/common/PageNotFound.vue
+++ socoolgames-client/src/components/common/PageNotFound.vue
@@ -1,8 +1,14 @@
 <template>
     <section>
-        <div class="text-center mt-15 my-10">
-            <h1>{{$t('pageNotFound.title')}}</h1>
+        <div class="text-center mt-15 mt-10">
+            <p class="underline">Oops...</p>
+        </div>
+        <div class="text-center mt-15 mt-5 mb-10">
+            <h1 class="mb-3">{{$t('pageNotFound.title')}}</h1>
             <p>{{$t('pageNotFound.description')}}</p>
+        </div>
+        <div class="text-center mt-15 my-10 bold">
+           <a class="button bg-primary p-3 rounded-xl text-white" href="javascript:history.go(-2)">{{$t('pageNotFound.back')}}</a>
         </div>
     </section>
 
