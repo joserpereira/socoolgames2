80ba1255de8cc7be7745fbbb3870f6f80889a313
Jose Pereira <jose.g.pereira@gmail.com>
2026-04-27 18:40:01 +0100

    changed: small layout issues added: 4 images fields

--- socoolgames-client/src/components/admin/cms/pageBuilder/Block4Columns.vue
+++ socoolgames-client/src/components/admin/cms/pageBuilder/Block4Columns.vue
@@ -1,34 +1,46 @@
 <template>
-    <section class="py-16 bg-white">
+    <section class="py-16 bg-gradient-to-r from-yellow-100 via-yellow-200 to-green-100 ">
         <div class="max-w-7xl mx-auto px-6 text-center">
 
-            <h2 class="text-3xl md:text-4xl font-bold mb-12">
-            {{ props.data.title?.[props.selectedLang] }}
-            </h2>
-            <h3 class="text-2xl md:text-2xl font-bold mb-12">
-            {{ props.data.subTitle?.[props.selectedLang] }}
-            </h3>
+            <h2 class="text-3xl md:text-4xl font-bold mb-8" v-html="props.data.title?.[props.selectedLang]"></h2>
+            <h3 class="text-2xl md:text-2xl mb-12" v-html="props.data.subTitle?.[props.selectedLang]"></h3>
             <div class="grid md:grid-cols-4 gap-8">
 
-            <div class="hover:scale-110">
-                <div class="text-4xl mb-4">{{ props.data.title1?.[props.selectedLang] }}</div>
-                <h3 class="font-bold mb-2">{{ props.data.subTitle1?.[props.selectedLang] }}</h3>
-            </div>
+                <div class="hover:scale-110 rounded-xl bg-white">
+                    <div v-if="props.data.image1?.[props.selectedLang].thumb" 
+                        class="flex flex-row justify-center items-center">
+                        <img class="h-16" :src="formatUrl(baseUrl + (props.data.image1?.[props.selectedLang].thumb))" />
+                    </div>
+                    <div v-if="props.data.title1?.[props.selectedLang]" class="text-4xl mb-4">{{ props.data.title1?.[props.selectedLang] }}</div>
+                    <h3 class="font-bold mb-2 mx-1">{{ props.data.subTitle1?.[props.selectedLang] }}</h3>
+                </div>
 
-            <div class="hover:scale-110">
-                <div class="text-4xl mb-4">{{ props.data.title2?.[props.selectedLang] }}</div>
-                <h3 class="font-bold mb-2">{{ props.data.subTitle2?.[props.selectedLang] }}</h3>
-            </div>
+                <div class="hover:scale-110 rounded-xl bg-white">
+                    <div v-if="props.data.image2?.[props.selectedLang].thumb" 
+                        class="flex flex-row justify-center items-center">
+                        <img class="h-16" :src="formatUrl(baseUrl + (props.data.image2?.[props.selectedLang].thumb))" />
+                    </div>
+                    <div v-if="props.data.title2?.[props.selectedLang]" class="text-4xl mb-4">{{ props.data.title2?.[props.selectedLang] }}</div>
+                    <h3 class="font-bold mb-2 mx-1">{{ props.data.subTitle2?.[props.selectedLang] }}</h3>
+                </div>
 
-            <div class="hover:scale-110">
-                <div class="text-4xl mb-4">{{ props.data.title3?.[props.selectedLang] }}</div>
-                <h3 class="font-bold mb-2">{{ props.data.subTitle3?.[props.selectedLang] }}</h3>
-            </div>
+                <div class="hover:scale-110 rounded-xl bg-white">
+                    <div v-if="props.data.image3?.[props.selectedLang].thumb" 
+                        class="flex flex-row justify-center items-center">
+                        <img class="h-16" :src="formatUrl(baseUrl + (props.data.image3?.[props.selectedLang].thumb))" />
+                    </div>
+                    <div v-if="props.data.title3?.[props.selectedLang]" class="text-4xl mb-4">{{ props.data.title3?.[props.selectedLang] }}</div>
+                    <h3 class="font-bold mb-2 mx-1">{{ props.data.subTitle3?.[props.selectedLang] }}</h3>
+                </div>
 
-            <div class="hover:scale-110">
-                <div class="text-4xl mb-4">{{ props.data.title4?.[props.selectedLang] }}</div>
-                <h3 class="font-bold mb-2">{{ props.data.subTitle4?.[props.selectedLang] }}</h3>
-            </div>
+                <div class="hover:scale-110 rounded-xl bg-white">
+                    <div v-if="props.data.image4?.[props.selectedLang].thumb" 
+                        class="flex flex-row justify-center items-center">
+                        <img class="h-16" :src="formatUrl(baseUrl + (props.data.image4?.[props.selectedLang].thumb))" />
+                    </div>
+                    <div v-if="props.data.title4?.[props.selectedLang]" class="text-4xl mb-4">{{ props.data.title4?.[props.selectedLang] }}</div>
+                    <h3 class="font-bold mb-2 mx-1">{{ props.data.subTitle4?.[props.selectedLang] }}</h3>
+                </div>
 
             </div>
         </div>
@@ -36,7 +48,9 @@
 </template>
 <script setup>
     import { defineProps } from 'vue';
-
+    import { formatUrl } from "@/utils/url.utils";
+    const baseUrl = process.env.VUE_APP_API_URL;
+    
     const props = defineProps({        
         data: {
             type: Object
--- socoolgames-client/src/components/admin/cms/pageBuilder/block4ColumnsData.ts
+++ socoolgames-client/src/components/admin/cms/pageBuilder/block4ColumnsData.ts
@@ -14,12 +14,16 @@ class BlockTextData {
         title: { pt: '', en: '' },
         subTitle: { pt: '', en: '' },
         title1: { pt: '', en: '' },
+        image1: { pt: '', en: '' },        
         subTitle1: { pt: '', en: '' },
         title2: { pt: '', en: '' },
+        image2: { pt: '', en: '' },        
         subTitle2: { pt: '', en: '' },
         title3: { pt: '', en: '' },
+        image3: { pt: '', en: '' },        
         subTitle3: { pt: '', en: '' },
         title4: { pt: '', en: '' },
+        image4: { pt: '', en: '' },        
         subTitle4: { pt: '', en: '' }
     }  }
 
@@ -32,13 +36,18 @@ class BlockTextData {
         { field: "title", type: "WidgetText", label: "Title" },
         { field: "subTitle", type: "WidgetMultiline", label: "Sub Title" },
         { field: "title1", type: "WidgetText", label: "Title 1" },
+        { field: "image1", type: "WidgetImage", label: "Image 1" },
         { field: "subTitle1", type: "WidgetMultiline", label: "Sub Title 1" },
         { field: "title2", type: "WidgetText", label: "Title 2" },
+        { field: "image2", type: "WidgetImage", label: "Image 2" },
         { field: "subTitle2", type: "WidgetMultiline", label: "Sub Title 2" },
         { field: "title3", type: "WidgetText", label: "Title 3" },
+        { field: "image3", type: "WidgetImage", label: "Image 3" },
         { field: "subTitle3", type: "WidgetMultiline", label: "Sub Title 3" },
         { field: "title4", type: "WidgetText", label: "Title 4" },
+        { field: "image4", type: "WidgetImage", label: "Image 4" },
         { field: "subTitle4", type: "WidgetMultiline", label: "Sub Title 4" }
+        
     ]
   }
   
