ceecba42694bbe2c1aa2c67d0f0903a67f87f0a3
Jose Pereira <jose.g.pereira@gmail.com>
2026-04-29 19:49:23 +0100

    changed hide console messages in production

--- socoolgames-client/src/services/common/api.ts
+++ socoolgames-client/src/services/common/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+if (process.env.NODE_ENV !== "production") {
   console.log("mode", process.env.NODE_ENV);
   console.log("API URL:", process.env.VUE_APP_API_URL);
-// console.log("API PORT:", process.env.VUE_APP_API_PORT);
+}
+
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL + "/api",
   headers: {
