c32a346b4edb87ce087d7d416bddaa00daf40df3
Jose Pereira <jose.g.pereira@gmail.com>
2026-05-04 18:57:25 +0100

    updated: UI

--- socoolgames-client/src/components/admin/cms/pageBuilder/BlockCarouselHero.vue
+++ socoolgames-client/src/components/admin/cms/pageBuilder/BlockCarouselHero.vue
@@ -9,20 +9,20 @@
     <section
       v-for="(item, i) in props.data.items"
       :key="i"
-      class="w-full px-6 flex-shrink-0 bg-gradient-to-r1 from-green-100 to-green-200 py-20 md:bg-cover bg-cover"
+      class="w-full h-[50rem] px-6 flex-shrink-0 py-20 md:bg-cover bg-cover bg-bottom"
       :style="getBgImage(item)"
     >
       <div class="px-16 max-w-7xl mx-auto grid md:grid-cols-2  items-center">
 
         <div>
-          <h1 class="text-4xl backdrop-blur-sm md:text-5xl font-black leading-tight rounded-lg  pt-4 inline-block">
+          <h1 class="text-4xl backdrop-blur-sm md:text-5xl font-black leading-tight rounded-lg p-4 inline-block">
             {{ item.title?.[props.selectedLang] }}
           </h1>
         </div>
         <div>
         </div>
         <div class="mb-6 py-4 ">
-          <p class="backdrop-blur-sm text-lg text-gray-700 mb-8 rounded-lg py-4 inline-block" v-html="formatText(item.subTitle?.[props.selectedLang])"></p>
+          <p class="backdrop-blur-sm font-medium text-lg text-gray-900 mb-8 rounded-lg p-4 inline-block" v-html="formatText(item.subTitle?.[props.selectedLang])"></p>
 
           <div>
             <a
@@ -42,14 +42,20 @@
     @click="prev"
     class="absolute left-4 top-1/2 -translate-y-1/2 w-12 h-12 rounded-full border border-white/40 text-white backdrop-blur flex items-center justify-center hover:bg-white/10"
   >
-    ◀
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+    </svg>
+
   </button>
 
   <button
     @click="next"
     class="absolute right-4 top-1/2 -translate-y-1/2 w-12 h-12 rounded-full border border-white/40 text-white backdrop-blur flex items-center justify-center hover:bg-white/10"
   >
-    ▶
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+    </svg>
+
   </button>
 
   <div class="absolute bottom-6 left-1/2 -translate-x-1/2 flex gap-3">
@@ -91,7 +97,7 @@
     const scheduleNext = () => {
         data.timeoutID = setTimeout(() => {
             next();
-        }, 3000)
+        }, 4000)
     }
     onMounted(() => {
         scheduleNext();
